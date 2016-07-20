08aebd645489ca83e5456a6b987e7bc9b6fefda8
JHthe4 <jakob.hildebrand@gmail.com>
2016-07-20 23:25:37 +0200

    Added settings menu

--- app/src/main/java/com/jakobhildebrand/garagenapp/MainActivity.java
+++ app/src/main/java/com/jakobhildebrand/garagenapp/MainActivity.java
@@ -1,13 +1,45 @@
 package com.jakobhildebrand.garagenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: Check if connected to Wifi and server is accessible
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onRefresh(){
+        //TODO: Check if connected to wifi and server is accessible
+    }
+
+    public void toggleGarage(View view){
+        //TODO: Implement volley request to yoctohub
+        //TODO: Implement singleton for Volley RequestQueue
     }
 }
