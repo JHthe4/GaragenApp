0bd75730d315ea98cdf5cdafff8552ae06472946
JHthe4 <jakob.hildebrand@gmail.com>
2016-09-16 17:08:40 +0200

    Removed volley singleton. Wrote networking code. First Beta

--- app/src/main/java/com/jakobhildebrand/garagenapp/MainActivity.java
+++ app/src/main/java/com/jakobhildebrand/garagenapp/MainActivity.java
@@ -8,28 +8,59 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-import java.io.IOException;
+import com.yoctopuce.YoctoAPI.YAPI;
+import com.yoctopuce.YoctoAPI.YAPI_Exception;
+import com.yoctopuce.YoctoAPI.YRelay;
+
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 
-public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class MainActivity extends AppCompatActivity{
     private SharedPreferences prefs;
     private String hostname;
     private int port;
+    private String relayName;
+    private SwipeRefreshLayout swipeLayout;
+    private YRelay mRelay;
+    private ImageButton myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        mRelay = null;
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeLayout.setRefreshing(true);
+                new RetrieveFeedTask(hostname, port, relayName).execute();
+                swipeLayout.setRefreshing(false);
+            }
+        });
+        swipeLayout.setColorSchemeColors(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
+        myButton = (ImageButton) findViewById(R.id.imageButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.isEnabled()) {
+                    toggleGarage();
+                }
+            }
+        });
     }
 
     @Override
@@ -38,9 +69,10 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         prefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
         hostname = prefs.getString("pref_hostname", "");
         port = Integer.parseInt(prefs.getString("pref_port", ""));
-        new RetrieveFeedTask(hostname, port).execute();
-        //TODO: Check if connected to Wifi and server is accessible
+        relayName = prefs.getString("pref_relayname", "");
+        new RetrieveFeedTask(hostname, port, relayName).execute();
     }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
@@ -57,57 +89,96 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         }
     }
 
-    public void onRefresh(){
-        new RetrieveFeedTask(hostname, port).execute();
-    }
-
-    public void toggleGarage(View view){
-        //TODO: Implement volley request to yoctohub
-        //TODO: Implement singleton for Volley RequestQueue
+    public void toggleGarage(){
+        new OpenGarageTask().execute();
     }
 
     private boolean isWifiOnAndConnected() {
         if (prefs.getBoolean("pref_use_mobile", true)) return true;
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm.getActiveNetworkInfo();
-        if (ni != null && ni.getType() == ConnectivityManager.TYPE_WIFI) {
-            return true;
-        } else {
-            return false;
-        }
+        return (ni != null && ni.getType() == ConnectivityManager.TYPE_WIFI);
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+        YAPI.FreeAPI();
+    }
+
+    //Checks if connected to wifi and if server is available
     class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
         private Exception exception;
         private boolean isServerAccessible;
         private String hostname;
         private int port;
+        private String relayName;
 
-        public RetrieveFeedTask(String hostname, int port){
+        public RetrieveFeedTask(String hostname, int port, String relayName){
             this.hostname = hostname;
             this.port = port;
+            this.relayName = relayName;
         }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //myButton.setEnabled(false);
+        }
+
+        @Override
         protected Void doInBackground(Void... params){
             try {
                 Socket socket = new Socket();
                 SocketAddress socketAddress = new InetSocketAddress(this.hostname, this.port);
                 socket.connect(socketAddress, 1000);
                 socket.close();
+                if(mRelay == null) {
+                    YAPI.RegisterHub(hostname + ":" + port + "/");
+                    mRelay = YRelay.FindRelay(relayName);
+                }
                 isServerAccessible = true;
             } catch (Exception e) {
                 this.exception = e;
+                e.printStackTrace();
                 isServerAccessible = false;
+                mRelay = null;
             }
             return null;
         }
 
+        @Override
         protected void onPostExecute(Void v) {
-            ImageButton myButton = (ImageButton) findViewById(R.id.imageButton);
             if (isWifiOnAndConnected() && isServerAccessible) {
                 myButton.setEnabled(true);
             } else {
                 myButton.setEnabled(false);
             }
+        }
+    }
+
+    class OpenGarageTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        public Void doInBackground(Void... params){
+            if(mRelay != null){
+                if(mRelay.isOnline()) {
+                    Log.i("Reached:", "relay is online");
+                    try {
+                        mRelay.pulse(500);
+                    } catch (YAPI_Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Relay is not online", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(MainActivity.this, "Couldn't reach relay", Toast.LENGTH_SHORT).show();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void v){
         }
     }
 }
